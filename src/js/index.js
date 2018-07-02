10de7a003d4c14cac078189fb0d16fd65807935c
uanoob <usnet@ukr.net>
2018-07-02 10:37:06 +0300

    Create search view module

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -10,21 +12,23 @@ const state = {};
 
 const controlSearch = async () => {
   // Get query from view
-  const query = 'pizza'; //TODO
+  const query = searchView.getInput();
   if (query) {
     // New search object and add to state
     state.search = new Search(query);
     // Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     // Search for recipes
     await state.search.getResults();
 
     // Render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
