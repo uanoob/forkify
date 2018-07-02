72b41c1c56b9b8b6f458bb582ba9dd5ba1d2f310
uanoob <usnet@ukr.net>
2018-07-02 11:48:03 +0300

    Add loader spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -19,11 +19,13 @@ const controlSearch = async () => {
     // Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // Search for recipes
     await state.search.getResults();
 
     // Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
