536526f457228e11a70fe380db5267d3df34bb15
uanoob <usnet@ukr.net>
2018-07-02 11:12:11 +0300

    Create limit recipe title function

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,20 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
+};
+
 const renderRecipe = recipe => {
   const markup = `
   <li>
@@ -20,7 +34,7 @@ const renderRecipe = recipe => {
         <img src="${recipe.image_url}" alt="${recipe.title}">
       </figure>
       <div class="results__data">
-        <h4 class="results__name">${recipe.title}</h4>
+        <h4 class="results__name">${limitRecipeTitle(recipe.title, 20)}</h4>
         <p class="results__author">${recipe.publisher}</p>
       </div>
     </a>
