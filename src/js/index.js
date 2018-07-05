7a409a8db98113dadf285c4d61864766db0a2889
uanoob <usnet@ukr.net>
2018-07-05 10:01:05 +0300

    Add format count to Recipe View & highlighting to Search View

--- src/js/index.js
+++ src/js/index.js
@@ -64,6 +64,10 @@ const recipeCtrl = async () => {
     // Prepare UI for changes
     recipeView.clearRecipe();
     renderLoader(elements.recipe);
+    // Highlight selected search item
+    if (state.search) {
+      searchView.highlightSelected(id);
+    }
     // Create new recipe object
     state.recipe = new Recipe(id);
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -13,6 +13,14 @@ export const clearResults = () => {
   elements.searchResPages.innerHTML = '';
 };
 
+export const highlightSelected = id => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link--active'));
+  resultsArr.forEach(item => {
+    item.classList.remove('results__link--active');
+  });
+  document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
+
 const limitRecipeTitle = (title, limit = 17) => {
   const newTitle = [];
   if (title.length > limit) {
